53e358c7bc0691531b8cdacce66a8e5472d7dd00
bigcd <bigcd@bbigcd.local>
2016-03-06 21:09:18 +0800

    网易新闻codereview

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BCDNews
+//
+//  Created by bigcd on 16/2/7.
+//  Copyright © 2016年 bbigcd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
